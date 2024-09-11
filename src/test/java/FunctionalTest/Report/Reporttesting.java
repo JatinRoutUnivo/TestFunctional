4a3a4d6df4d8af4fc846be4c7abaf901cf839c7b
JatinRoutUnivo <jrout@univoedu.com>
2024-09-12 02:19:43 +0530

    Update Reporttesting.java

--- src/test/java/FunctionalTest/Report/Reporttesting.java
+++ src/test/java/FunctionalTest/Report/Reporttesting.java
@@ -37,7 +37,9 @@ public class Reporttesting {
 	@BeforeTest
 	public void GenerateReport() {
 		
-		reporter=new ExtentHtmlReporter("C:\\Users\\admin\\eclipse-workspace\\Report\\ExtentReport\\FunctionalTestReport.html");
+
+		String jenkinsWorkspacePath = System.getProperty("user.dir") + "/ExtentReport/FunctionalTestReport.html";
+	    reporter = new ExtentHtmlReporter(jenkinsWorkspacePath);
 		reporter.config().setTheme(Theme.STANDARD);
 		reporter.config().setReportName("Functional Test report");
 		reporter.config().setTimeStampFormat("dd-MM-yyyy HH:mm");
